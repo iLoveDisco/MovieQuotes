e3aca44a8ebce3ff3a863b6452eec284a5519eff
Eric tu <tuey@rose-hulman.edu>
2021-01-17 22:05:24 -0500

    Converted table to show movie quotes

---
+++ MovieQuotes/MovieQuotes.swift
@@ -0,0 +1,19 @@
+//
+//  MovieQuotes.swift
+//  MovieQuotes
+//
+//  Created by Eric Tu on 1/17/21.
+//
+
+import UIKit
+class MovieQuote {
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String) {
+        self.quote = quote
+        self.movie = movie
+    }
+    
+    
+}
--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -9,15 +9,25 @@ import UIKit
 
 class MovieQuotesTableViewController: UITableViewController {
     let movieQuoteCellId = "MovieQuoteCell"
-    var names = ["Eric", "Jun", "James", "Angela"]
+    var movieQuotes = [MovieQuote]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        movieQuotes.append(MovieQuote(quote: "It's not about the money, it's about sending a message", movie: "The Dark Knight Rises"))
+        movieQuotes.append(MovieQuote(quote: "Now's not a good time to be a Nazi", movie: "Jo Jo Rabbit"))
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return movieQuotes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: movieQuoteCellId, for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        // Configure the cell
+        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+        cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
+        
         return cell
     }
 }

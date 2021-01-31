746d8472ff1028af683691f374a7fcc92d054386
Eric tu <tuey@rose-hulman.edu>
2021-01-31 00:31:44 -0500

    added sign in and log in

--- MovieQuotes/LoginViewController.swift
+++ MovieQuotes/LoginViewController.swift
@@ -6,18 +6,40 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class LoginViewController: UIViewController {
     let showListSegueID = "ShowListSegue"
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     @IBAction func pressedRegisterButton(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error creating a new user for email/password \(error)")
+                return
+            }
+            print("New user is created with email \(authResult?.user.email)")
+            self.performSegue(withIdentifier: self.showListSegueID, sender: self)
+        }
     }
     
-    
     @IBAction func pressedLoginButton(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error creating a new user for email/password \(error)")
+                return
+            }
+            print("User is signed in with email \(authResult?.user.email)")
+            self.performSegue(withIdentifier: self.showListSegueID, sender: self)
+        }
     }
 }
